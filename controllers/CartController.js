aa6236e171cde2bb02d94a933645cbb62816a6f1
Ceo-Sammarco <noah.kd@gmail.com>
2023-03-24 12:46:48 -0300

    new commit sex 24 mar 2023 12:46:48 -03

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -1,8 +1,15 @@
 
 
+const { addProduct2Cart } = require("../models/Cart"),
+    { getProductById } = require( "../models/Products" );
+
 exports.postCartPage = ( req, res ) => {
     // console.log( req.body );
     const productId = req.body.productId;
+    getProductById( productId, product => {
+        addProduct2Cart( productId, product.price );
+        res.redirect( "/" );
+    } );
 };
 
 exports.getCartPage = ( req, res ) => {
--- models/Cart.js
+++ models/Cart.js
@@ -4,9 +4,31 @@ const fs = require( "fs" ),
     path = require( "path" ),
     rootDir = require( "../utils/path" );
 
-exports.addProduct2Cart = productId => {
+exports.addProduct2Cart = ( productId, productPrice ) => {
     const cartPath = path.join( rootDir, "../data", "cart.json" );
     fs.readFile( cartPath, ( error, cartContent ) => {
-        
+        let cart = { products: [], totalPrice: 0 };
+        if( !error ) {
+            cart = JSON.parse( cartContent );
+        }
+
+        let existingProductIndex = cart.products.findIndex( p => p.id.toString() == productId );
+
+        let updatedProduct;
+
+        if( existingProductIndex != -1 ) {
+            updatedProduct = [ ...cart.products[ existingProductIndex ] ];
+            updatedProduct.quantity += 1;
+            cart.products = [ ...cart.products ];
+            cart.products = [ existingProductIndex ] = updatedProduct;
+        } else {
+            updatedProduct = { id: productId, quantity: 1 };
+            cart.products = [ ...cart.products, updatedProduct ];
+        }
+
+        cart.totalPrice += +productPrice;
+        fs.writeFile( cartPath, JSON.stringify( cart ), error => {
+            console.log( error );
+        } ); 
     } );
 };
